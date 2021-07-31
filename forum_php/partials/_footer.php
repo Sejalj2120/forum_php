fcc4d487cd3b31c27608fbf7c604ac514d99f40b
Sejal Jagtap <60838230+Sejalj2120@users.noreply.github.com>
2021-07-31 15:23:37 +0530

    Delete forum_php directory

--- forum_php/partials/_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="container-fluid bg-dark text-light">
-<p class= "text-center mb-0">Copyright Cummins Forum 2021 | All right reserved</p>    <!-- mb : margin bottom , fluid is full bg-->
-</div>
